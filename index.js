b7d01e53d422207c6d415fb0db4cf3ae1a304388
joaoreider <joaopauloj405@gmail.com>
2023-02-20 10:58:30 -0300

    acessando dados dos forms

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize');
 
 
@@ -12,6 +13,12 @@ const Sequelize = require('sequelize');
     //template engine
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+    
+    // Body Parser
+    app.use(express.json());
+    app.use(express.urlencoded({
+    extended: true
+    }));
 
     // conexão banco de dandos PostgreSQL
     const sequelize = new Sequelize('testesequelize', 'postgres', '123', {
@@ -23,6 +30,11 @@ const Sequelize = require('sequelize');
 // Rotas
 app.get("/cadastro", function(req, res){
     res.render('formulario')
+});
+
+app.post("/cadastro_enviado", function(req, res){
+
+    res.send(`Título: ${req.body.titulo}\nConteúdo: ${req.body.conteudo} `)
 });
 
 
